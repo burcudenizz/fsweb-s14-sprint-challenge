1d80989afcefab08d11819100727583fa9941246
Burcu Deniz <dnzzburcu@gmail.com>
2023-05-26 22:32:46 +0300

    server çalıştırıldı.

--- api/server.js
+++ api/server.js
@@ -1,1 +1,8 @@
 // serverı buraya yazın ve index.js require yazın
+
+const express =require("express"); // 1-expressi getirdik.
+const server =express(); //2-expressi servera bağladık.
+
+server.use(express.json());
+
+module.exports = server;
---
+++ data/seeds/projects.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+
+
+
+ 
+  const defaultProjects=[{project_name:"React ile Web Sitesi Tasarlama",project_description:"React kütüphanesi ile form ekranı oluşturma"}]
+
+
+
+
+  
+};
